7d132a89bcb9517a0df26b983c14579d20e6f58b
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-09-27 16:49:56 +0300

    start

---
+++ Python_lecture/lec1/lec.py
@@ -0,0 +1,48 @@
+# c = None
+# a = 123
+# b = 1.23
+# print(a)
+# print(b)
+# print(c)
+# c = 111
+# print(c)
+# s = '\'Hello\''
+# print(s)
+# print(a, '-', b, c, s)
+# print('{2} - {1} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = ['1', '2', '3']
+# print(list)
+
+
+# print('enter ')
+# new = input()
+# print(f'new = {new}')
+
+# a = 1.3
+# b = 3
+# c = round (a * b, 3) 
+
+# # // целочисленное деление
+# # % получяаем остаток
+# # ** возведение в степень
+# print(c)
+
+# a = 1 < 4 or 5 < 2
+# print(a)
+
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+
+
+
+
