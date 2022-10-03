56faf1e59196b5be5a49c27c2c8928cfa3cc302f
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-03 19:49:35 +0300

    sem 2

---
+++ Python_work/python_work_1.py
@@ -0,0 +1,12 @@
+# number = int(input('Enter N: '))
+# list = [None] * number * 2 + [None]
+# j = 0
+# for i in range(-number, number + 1):
+#     list[j] = i
+#     j += 1
+
+# print (list)
+
+a = float(input())
+b = int((a * 10) % 10)
+print(b)
---
+++ Python_work/work_2.py
@@ -0,0 +1,21 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# newlist = [a, b, c]
+# count = 0
+# for i in range(2):
+#     for j in range (i, 2):
+#         if newlist[i] == newlist[i + 1]:
+#             count += 1
+# print(count)
+
+
+# a = int(input())
+# b = int(input())
+# b = b + (b + 1)%2
+# k = []
+# for i in range(b, a + 1, 2): k.append(i)
+# k = k [:: -1] # это срез, т.е. [навало среза : конец среза : шаг среза]
+# print(k)
+
+
