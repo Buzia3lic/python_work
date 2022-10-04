6b6a9bfecda17396ee2a7dfafe9ec17d9971f05b
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-04 17:08:34 +0300

    sem2_done

--- Python_work/work_2.py
+++ Python_work/work_2.py
@@ -19,3 +19,31 @@
 # print(k)
 
 
+# N = int(input('Введите N: '))
+# k = [1]
+# for i in range(1, N):
+#     k.append(k[i - 1] * (-3))
+# print(k)
+
+# num = input('Введите N: ')
+# if num [:1] == num [:3: -1]:
+#     print('yes')
+# else:
+#     print('No')
+
+# num = int(input('Введите N: '))
+# num = (num // 100) * 10 + num % 10 
+# print(num)
+
+
+# str1 = input('Введите строку 1: ')
+# str2 = input('Введите строку 2: ')
+# # count = 0
+# # for i in range(len(str1)):
+# #     for j in range(len(str2)):
+# #         if i == j:
+# #             count += 1
+# # print(count)
+# print(str1.count(str2))
+
+
