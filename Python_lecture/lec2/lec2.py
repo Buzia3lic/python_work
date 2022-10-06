db0df569ebcb6943d5a70d6982895a8f17c2dbea
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-06 14:25:55 +0300

    lec2

---
+++ Python_lecture/lec2/lec2.py
@@ -0,0 +1,30 @@
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'w')
+# data.writelines(colors)
+# data.write('\nLINE 2\n') # \n переход на новую строку
+# data.close()
+
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+
+
+# t = tuple(['red', 'green', 'blue'])
+# red, green, blue = t
+# print('r: {} g: {} b: {}'.format(red, green, blue))
+
+
+# dictionary = {}
+# dictionary = \
+#     {
+#         'up' : 'w',
+#         'down' : 's'
+#     }
+# print(dictionary)
+# print(dictionary['up'])
+
+
