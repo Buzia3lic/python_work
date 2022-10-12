03b5e0d9f369a2eb0be9618c997786883e3163a0
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-13 00:14:23 +0300

    work4

--- Python_work/work_4.py
+++ Python_work/work_4.py
@@ -1,7 +1,25 @@
-list = [int(i) for i in input('Введите числа через пробел: ').split()]
-print(list)
+# list = [int(i) for i in input('Введите числа через пробел: ').split()]
+# print(list)
 
-a = max(list)
-b = min(list)
-print(a, b)
+# a = max(list)
+# b = min(list)
+# print(a, b)
+
+
+dict = \
+    {
+        'Hi':'Hello',
+        'Bye':'Goodbye',
+        'List':'Array'
+    }
+print(dict)
+
+find = 'Goodbye'
+
+if find in dict.keys():
+    print(dict[find])
+
+if find in dict.values():
+    ind = list(dict.values()).index(find)
+    print(list(dict.keys())[ind])
 
