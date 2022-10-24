76c207e3066e4bb910b84a25c3b25c3d4c8b96cc
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-10-24 22:02:18 +0300

    sem5

---
+++ Python_work/work_5.py
@@ -0,0 +1,19 @@
+# data = list(map(int, input().split()))
+
+# print(data)
+
+# for i in range(len(data)-1):
+#     if (data[i+1] - data[i]) != 1:
+#         print(data[i] + 1)
+
+#################
+
+# data = [i for i in range(10, 100)]
+# data1 = list(filter(lambda x: not x%9, data))
+# data2 = list(map(lambda x: x**2 ,filter(lambda x: not x%9, data)))
+# print(data1)
+# print(data2)
+# print(sum(data2))
+
+#################
+
